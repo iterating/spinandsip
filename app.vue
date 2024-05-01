12dc179aa4996145d933f378f593640f3e9984e3
jy <jy>
2024-04-30 19:43:58 -0700

    Replace boilerplate

--- app.vue
+++ app.vue
@@ -1,27 +1,27 @@
 <script setup>
-const { data } = await useAsyncData('indexx', () => queryContent('/indexx').findOne())
+const { data: page } = await useAsyncData('index', () => queryContent('/indexx').findOne())
 </script>
  
 
 <template>
   <main>
     <ContentRenderer :value="data">
-      <div v-if="data">
-      <h1>{{ data.title }}</h1>
+      <div v-if="page">
+      <h1>{{ page.title }}</h1>
       </div>
 
  <div 
-    class="flex flex-col items-center px-16 pt-10 pb-6 w-full bg-neutral-50 max-md:px-5 max-md:max-w-full" v-if="data">
+    class="flex flex-col items-center px-16 pt-10 pb-6 w-full bg-neutral-50 max-md:px-5 max-md:max-w-full" v-if="page">
 
   >
     <div class="flex flex-col max-w-full w-[918px]">
-      <div class="self-center text-3xl leading-10 text-black max-md:max-w-full" v-if="data">
-{{ data.hero.title }}      </div>
+      <div class="self-center text-3xl leading-10 text-black max-md:max-w-full" v-if="page">
+{{ page.hero.title }}      </div>
       <div
         class="mt-7 text-base font-bold leading-6 text-center text-neutral-800 max-md:max-w-full"
       >
       <img src="/public/landing.jpeg">
-{{ data.hero.description }}
+{{ page.hero.description }}
 
       </div>
     </div>
@@ -33,41 +33,41 @@ const { data } = await useAsyncData('indexx', () => queryContent('/indexx').find
       </div>
 
 
-    <div class="landing-" v-if="data">
-      <h1>{{ data.hero.title }}</h1>
-      <p>{{ data.hero.description }}</p>
+    <div class="landing-" v-if="page">
+      <h1>{{ page.hero.title }}</h1>
+      <p>{{ page.hero.description }}</p>
       <ul>
-        <li v-for="(link, index) in data.hero.links" :key="index">
+        <li v-for="(link, index) in page.hero.links" :key="index">
           <a :href="link.url">{{ link.text }}</a>
         </li>
       </ul>
-      <template v-if="data.hero.headline">
-        <div class="headline-badge relative rounded-full font-semibold" v-if="data">
-          <a :href="data.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
+      <template v-if="page.hero.headline">
+        <div class="headline-badge relative rounded-full font-semibold" v-if="page">
+          <a :href="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
             <span class="absolute inset-0" aria-hidden="true"></span>
           </a>
-          {{ data.hero.headline.label }}
-          <span v-if="data.hero.headline.icon" class="ml-1 w-4 h-4"></span>
+          {{ page.hero.headline.label }}
+          <span v-if="page.hero.headline.icon" class="ml-1 w-4 h-4"></span>
         </div>
       </template>
     </div>
 
 
 
-    <div v-for="(section, index) in data.sections" :key="index" class="landing-section" v-if="data">
+    <div v-for="(section, index) in page.sections" :key="index" class="landing-section" v-if="page">
       <h2>{{ section.title }}</h2>
       <p>{{ section.description }}</p>
       <div v-for="(feature, index) in section.features" :key="index">
-        <placeholder />
+
       </div>
     </div>
 
-    <div v-if="data">
-      <h2>{{ data.features.title }}</h2>
-      <p>{{ data.features.description }}</p>
+    <div v-if="page">
+      <h2>{{ page.features.title }}</h2>
+      <p>{{ page.features.description }}</p>
       <div class="u-data-grid">
-        <div v-for="(item, index) in data.features.items" :key="index" class="u-landing-card">
-          <placeholder />
+        <div v-for="(item, index) in page.features.items" :key="index" class="u-landing-card">
+
         </div>
       </div>
     </div>
@@ -99,15 +99,15 @@ const { data } = await useAsyncData('indexx', () => queryContent('/indexx').find
    
           
 
-  <div v-if="data">
-    <h2>{{ data.testimonials.title }}</h2>
-      <p>{{ data.testimonials.description }}</p>
-      <div class="u-data-columns" v-if="data">
-        <div v-for="(testimonial, index) in data.testimonials.items" :key="index" class="break-inside-avoid">
-          <div class="bg-gray-100/50 dark:bg-gray-800/50" v-if="data">
+  <div v-if="page">
+    <h2>{{ page.testimonials.title }}</h2>
+      <p>{{ page.testimonials.description }}</p>
+      <div class="u-data-columns" v-if="page">
+        <div v-for="(testimonial, index) in page.testimonials.items" :key="index" class="break-inside-avoid">
+          <div class="bg-gray-100/50 dark:bg-gray-800/50" v-if="page">
             <h3>{{ testimonial.title }}</h3>
-            <p>{{ testimonial.content }}</p>
-            <div class="testimonial-author" v-if="data">
+            <p>{{ testimonial.quote }}</p>
+            <div class="testimonial-author" v-if="page">
               <img :src="testimonial.author.avatar" :alt="testimonial.author.name" />
               <span>{{ testimonial.author.name }}</span>
             </div>
@@ -115,9 +115,7 @@ const { data } = await useAsyncData('indexx', () => queryContent('/indexx').find
         </div>
       </div>
 
-    <div class="landing-section">
-      <u-landing-cta v-bind="data.cta" class="bg-gray-100/50 dark:bg-gray-800/50"></u-landing-cta>
-    </div>
+
   </div>
 
   </ContentRenderer>
@@ -183,13 +181,3 @@ const { data } = await useAsyncData('indexx', () => queryContent('/indexx').find
   margin-right: 10px;
 }
 </style>
-
-<script>
-import hero from '~/pages/hero.vue';
-
-export default {
-  components: {
-    hero,
-  },
-};
-</script>
