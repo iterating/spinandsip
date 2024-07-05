f726f25da78ed402a99ef8bb11ce83c7cc420bdc
jy <jy>
2024-07-05 10:30:10 -0700

    reinit nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@vueuse/nuxt',
-    '@builder.io/sdk-vue/nuxt',
-    '@nuxtjs/tailwindcss',
-  ],
-  content: {
-    documentDriven: true
-  },
-  hooks: {
-    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter((c) => ['UButton'].includes(c.pascalName))
-
-      globals.forEach((c) => c.global = true)
-    }
-  },
-
-
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', "@nuxt/image"],
+  routeRules: {
+    '/': { prerender: true }
+  }
 })
