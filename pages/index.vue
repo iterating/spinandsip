6e97c5dfda5fd14d39535f1e5d25d0a52b49cee8
jy <jy>
2024-08-01 16:32:08 -0700

    Cleaned up homepafe

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+  //extract the `data` into variable `page`
 const { data: page } = await useAsyncData('index', () => queryContent('/indexx').findOne())
 </script> 
 
@@ -9,18 +10,7 @@ const { data: page } = await useAsyncData('index', () => queryContent('/indexx')
     <h1>{{ page.title }}</h1>
   </div>
 
-  <div 
-    class="flex flex-col items-center px-16 pt-10 pb-6 w-full bg-neutral-50 max-md:px-5 max-md:max-w-full" v-if="page">
-    <div class="flex flex-col max-w-full w-[918px]">
-      <div class="self-center text-3xl leading-10 text-black max-md:max-w-full" v-if="page">
-      {{ page.hero.title }} </div>
-    <div class="mt-7 text-base font-bold leading-6 text-center text-neutral-800 max-md:max-w-full">
-      <img src="/public/landing.jpeg">
-      {{ page.hero.description }}
-    </div>
-    </div>
-  </div>
-
+<!-- Hero -->
   <div class="landing text-center" v-if="page">
     <h1>{{ page.hero.title }}</h1>
     <p>{{ page.hero.description }}</p>
@@ -29,19 +19,19 @@ const { data: page } = await useAsyncData('index', () => queryContent('/indexx')
         <a :href="link.url">{{ link.text }}</a>
       </li>
     </ul>
-    <img src="/public/bar.spinandsiplogo.jpg">
     <template v-if="page.hero.headline">
       <div class="headline-badge relative rounded-full font-semibold" v-if="page">
         <a :href="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
           <span class="absolute inset-0" aria-hidden="true"></span>
         </a>
-        {{ page.hero.headline.label }}
         <span v-if="page.hero.headline.icon" class="ml-1 w-4 h-4"></span>
 
       </div>
     </template>
-  </div>
+  </div> 
 
+    <img src="/public/landing.jpeg">
+    <img src="/public/bar.spinandsiplogo.jpg">
 
   <div v-for="(section, index) in page.sections" :key="index" class="landing-section" v-if="page">
     <h2>{{ section.title }}</h2>
@@ -60,25 +50,6 @@ const { data: page } = await useAsyncData('index', () => queryContent('/indexx')
       </div>
     </div>
   </div>
-
-  <div class="flex flex-col justify-center w-full max-md:max-w-full">
-    <div class="flex flex-col justify-center w-full max-md:max-w-full">
-      <div class="flex flex-col items-center px-16 pt-9 pb-14 mt-1.5 w-full bg-stone-400 max-md:px-5 max-md:max-w-full">
-        <div class="flex flex-col max-w-full w-[993px]">
-          <div class="max-md:max-w-full">
-            <div class="flex gap-5 max-md:flex-col max-md:gap-0">
-              <div class="flex flex-col w-[29%] max-md:ml-0 max-md:w-full">
-                <div
-                  class="flex flex-col mt-14 text-xl leading-10 text-center text-black max-md:mt-10">
-                 
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
    
     <div class="text-center" v-if="page">
       <h2>{{ page.testimonials.title }}</h2>
