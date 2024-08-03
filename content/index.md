166bb322265d551cd3dd51b45b1b9c6c1babcbe5
jy <jy>
2024-08-03 16:39:03 -0700

    contentdoc

--- content/index.md
+++
@@ -1,7 +0,0 @@
----
-title: Spin and Sip - LA Bartender and DJ
-description: Spin and Sip is Bartender Jinn and DJ Remedy providing premium event bartender and DJ services in the greater Los Angeles Area
----
-![](landing.png)
-# Spin and Sip - LA Bartender and DJ
-Bartending and Dj Services by Jinn and DJ Remedy
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,83 @@
 <script setup>
   //extract the `data` into variable `page`
 const { data: page } = await useAsyncData('page-data', () => queryContent('/indexx').findOne())
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
+
+useSeoMeta({
+  titleTemplate: '',
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
 </script> 
 
 <template>
-<main>
-  <div v-if="page">
-    <h1>{{ page.title }}</h1>
-  </div>
+  <main>
+    <div v-if="page">
+      <h1>{{ page.title }}</h1>
+    </div>
 
-<!-- Hero -->
-    <h1>{{ page.hero.title }}</h1>
-    <p>{{ page.hero.description }}</p>
-    <ul>
-      <li v-for="(link, index) in page.hero.links" :key="index">
-        <a :href="link.url">{{ link.text }}</a>
-      </li>
-    </ul>
-    <template v-if="page.hero.headline">
-      <div class="headline-badge relative rounded-full font-semibold" v-if="page">
-        <a :href="page.hero.headline.to" target="_blank" class="focus:outline-none">
-          <span class="absolute inset-0" aria-hidden="true"></span>
-        </a>
-        <span v-if="page.hero.headline.icon" class="ml-1 w-4 h-4"></span>
+    <!-- Hero -->
+      <h1>{{ page.hero.title }}</h1>
+      <p>{{ page.hero.description }}</p>
+      <ul>
+        <li v-for="(link, index) in page.hero.links" :key="index">
+          <a :href="link.url">{{ link.text }}</a>
+        </li>
+      </ul>
+      <template v-if="page.hero.headline">
+        <div class="headline-badge relative rounded-full font-semibold" v-if="page">
+          <a :href="page.hero.headline.to" target="_blank" class="focus:outline-none">
+            <span class="absolute inset-0" aria-hidden="true"></span>
+          </a>
+          <span v-if="page.hero.headline.icon" class="ml-1 w-4 h-4"></span>
 
+        </div>
+      </template>
+
+      <img src="/public/landing.png">
+      <img src="/public/bar.spinandsiplogo.png">
+
+    <!-- Features -->
+      <div v-for="(section, index) in page.sections" :key="index" class="landing-section" v-if="page">
+        <h2>{{ section.title }}</h2>
+        <p>{{ section.description }}</p>
+        <div v-for="(feature, index) in section.features" :key="index"
+        </div>
       </div>
-    </template>
 
-    <img src="/public/landing.png">
-    <img src="/public/bar.spinandsiplogo.png">
+      <div v-if="page">
+        <h2>{{ page.features.title }}</h2>
+        <p>{{ page.features.description }}</p>
+        <div class="u-data-grid">
+          <div v-for="(item, index) in page.features.items" :key="index" class="u-landing-card">
 
-  <div v-for="(section, index) in page.sections" :key="index" class="landing-section" v-if="page">
-    <h2>{{ section.title }}</h2>
-    <p>{{ section.description }}</p>
-    <div v-for="(feature, index) in section.features" :key="index"
-    </div>
-  </div>
-
-  <div v-if="page">
-    <h2>{{ page.features.title }}</h2>
-    <p>{{ page.features.description }}</p>
-    <div class="u-data-grid">
-      <div v-for="(item, index) in page.features.items" :key="index" class="u-landing-card">
-
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-   
-    <div class="text-center" v-if="page">
-      <h2>{{ page.testimonials.title }}</h2>
-        <p>{{ page.testimonials.description }}</p>
-    </div>
-        <div class="u-data-columns" v-if="page">
-          <div v-for="(testimonial, index) in page.testimonials.items" :key="index" class="break-inside-avoid">
-            <div class="bg-gray-100/50 dark:bg-gray-800/50" v-if="page">
-              <h3>{{ testimonial.title }}</h3>
-              <p>{{ testimonial.quote }}</p>
-              <div class="testimonial-author" v-if="page">
-                <img :src="testimonial.author.avatar" :alt="testimonial.author.name" />
-                <span>{{ testimonial.author.name }}</span>
-              </div>
+     
+    <!-- Testimonials -->
+      <div class="text-center" v-if="page">
+        <h2>{{ page.testimonials.title }}</h2>
+          <p>{{ page.testimonials.description }}</p>
+      </div>
+      <div class="u-data-columns" v-if="page">
+        <div v-for="(testimonial, index) in page.testimonials.items" :key="index" class="break-inside-avoid">
+          <div class="bg-gray-100/50 dark:bg-gray-800/50" v-if="page">
+            <h3>{{ testimonial.title }}</h3>
+            <p>{{ testimonial.quote }}</p>
+            <div class="testimonial-author" v-if="page">
+              <img :src="testimonial.author.avatar" :alt="testimonial.author.name" />
+              <span>{{ testimonial.author.name }}</span>
             </div>
           </div>
         </div>
+      </div>
 
-</main>
+  </main>
 </template>
 
 <style scoped>
